51a61dd8c6ce89e5313ebfee8a61246cdd674352
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-24 11:33:22 -0500

    added different print options

--- manager_fixed.rb
+++ manager_fixed.rb
@@ -10,7 +10,7 @@ class Employee
   end
 
   def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year." "And current active status is #{@active}."
   end
 
   def give_annual_raise
@@ -34,9 +34,32 @@ class Manager < Employee
     # use email sending library...
     puts "Email sent!"
   end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
+
+  # i =  0
+  # while i < @employees.length
+  # @employees[i].active = false
+  # 
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
-manager.print_info
-manager.send_report
 
+manager.print_info
+manager.give_all_raises
+employee1.print_info
+employee2.print_info
+manager.fire_all_employees
+employee1.print_info
+employee2.print_info
+manager.send_report
